27a8dc52a4f41abdcd654c6db6ad4bdf9449e84c
Jesus Cervantes <jesusuper9@gmail.com>
2023-12-09 08:49:35 -0600

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,30 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
+  },
+  {
+    path: 'verify-email',
+    loadChildren: () => import('./verify-email/verify-email.module').then( m => m.VerifyEmailPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'password-reset',
+    loadChildren: () => import('./password-reset/password-reset.module').then( m => m.PasswordResetPageModule)
+  },
+  {
+    path: 'new-playlist',
+    loadChildren: () => import('./new-playlist/new-playlist.module').then( m => m.NewPlaylistPageModule)
+  },
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB7qR6O6bTnqvit-9zepaVh0euFWoOjIbQ',
+    authDomain: 'ionic-spotify.firebaseapp.com',
+    projectId: 'ionic-spotify',
+    storageBucket: 'ionic-spotify.appspot.com',
+    messagingSenderId: '1080031526035',
+    appId: '1:1080031526035:web:9a9c40aca2dda936a8a166',
+    measurementId: 'G-4M6F2TFKHT',
+  },
 };
 
 /*
